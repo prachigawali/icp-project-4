baf8442bc6ba39c4243b3cf72af8fbb9cf1baea3
rakeshhatzade11 <hatzaderakesh1@gmail.com>
2024-02-24 16:42:30 +0530

    all changes done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,9 +12,9 @@ const Footer = () => {
       <div className="f-content">
         <span>Portfoliocode@gmail.com</span>
         <div className="f-icons">
-          <Insta color="white" size={"3rem"} />
-          <Facebook color="white" size={"3rem"} />
-          <Gitub color="white" size={"3rem"} />
+          <a href="https://www.instagram.com/"><Insta color="white" size={"3rem"} /></a>
+          <a href="https://www.facebook.com/campaign/landing.php?campaign_id=14884913640&extra_1=s%7Cc%7C550525804791%7Cb%7Cfacebook%7C&placement=&creative=550525804791&keyword=facebook&partner_id=googlesem&extra_2=campaignid%3D14884913640%26adgroupid%3D128696220912%26matchtype%3Db%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-592856129%26loc_physical_ms%3D9299863%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gad_source=1&gclid=Cj0KCQiAxOauBhCaARIsAEbUSQQOJ9NPvk9OD7gSlrJi76x7zL4A3r-34LlXsWpqUcvhu9diXtbKojUaArMJEALw_wcB"><Facebook color="white" size={"3rem"} /></a>
+         <a href="https://github.com/rakeshhatzade11/icp-project-4/tree/main"> <Gitub color="white" size={"3rem"} /></a>
         </div>
       </div>
     </div>
